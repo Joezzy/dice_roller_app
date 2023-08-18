0fa5febb682c3647d42286d78c0d47d95c7b18af
joezzy <onalojoseph96@gmail.com>
2023-08-18 18:40:05 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dice_roller_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+
+    );
+  }
+}
